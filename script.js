5887277d56429b8fc0ce9cba8d6e84f01ca62ae0
Nelson de Barros <nelsinho.527@gmail.com>
2023-02-20 10:56:44 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const gameBoard = (() => {
+    const gameboard = [];
+    return {
+        gameboard,
+    };
+})();
+
+// const displayController = (() => {
+//     //
+//     return {
+
+//     };
+// })();
+
+const Player = (name) => {
+    
+    return {
+
+    };
+};
